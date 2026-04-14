7b4416703c8b219ede1d8c16c0e613d5319e0e13
Kiran Jaya Prakash <93246187+Kira138@users.noreply.github.com>
2026-04-14 13:04:28 +0530

    W6: CSV upload with mode switching

--- app/main.py
+++ app/main.py
@@ -51,6 +51,7 @@ page = st.sidebar.radio(
 st.sidebar.markdown("---")
 st.sidebar.subheader("📁 Upload Your Data")
 uploaded_file = st.sidebar.file_uploader("Upload CSV file", type=['csv'])
+accept_multiple_files=True 
 if uploaded_file is None:
    col1, col2 , col3, col4 = st.columns(4)
    col_left, col_right = st.columns(2)
